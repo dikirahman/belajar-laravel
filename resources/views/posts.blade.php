92d97007327344976358db303f9b81f2ee68b374
Diki Rahman <dikirahman.me@gmail.com>
2021-07-24 11:56:52 +0700

    add link to single post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,4 +2,12 @@
 
 @section('container')
     <h1>Halaman Blog</h1>
+
+    @foreach ($posts as $post)
+        <article class="mb-5">
+            <h2><a href="/post/{{ $post['slug'] }}">{{ $post["title"] }}</a></h2>
+            <h5>{{ $post["author"] }}</h5>
+            <p>{{ $post["body"] }}</p>
+        </article>
+    @endforeach
 @endsection
