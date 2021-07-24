dfba2aaa42dd49fde4c0288c3d539af5e711bec9
Diki Rahman <dikirahman.me@gmail.com>
2021-07-24 13:19:14 +0700

    delete closure get single data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,52 +32,18 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Diki Rahman",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magni dolorum neque ut, quaerat, hic esse itaque autem dicta illo corrupti sit molestias quibusdam reprehenderit tenetur porro beatae inventore tempore aliquid obcaecati. Accusantium quis tempora accusamus laudantium sed voluptatem debitis aliquam dolores. Assumenda adipisci molestiae error pariatur recusandae doloribus itaque ratione nemo placeat optio debitis, aliquid animi illo quisquam dicta quasi eos saepe, atque quo repudiandae, laboriosam cumque rerum et! Nulla quo quidem earum! Omnis similique accusamus dicta rerum laudantium?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Diki Rahman",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magni dolorum neque ut, quaerat, hic esse itaque autem dicta illo corrupti sit molestias quibusdam reprehenderit tenetur porro beatae inventore tempore aliquid obcaecati. Accusantium quis tempora accusamus laudantium sed voluptatem debitis aliquam dolores. Assumenda adipisci molestiae error pariatur recusandae doloribus itaque ratione nemo placeat optio debitis, aliquid animi illo quisquam dicta quasi eos saepe, atque quo repudiandae, laboriosam cumque rerum et! Nulla quo quidem earum! Omnis similique accusamus dicta rerum laudantium? laboriosam cumque rerum et! Nulla quo quidem earum! Omnis similique accusamus dicta rerum laudantium?"
-        ],
-        ];
+    
     return view('posts', [
         "title" => "Post",
-        "posts" => $blog_posts
+        "posts" => Post::all()
     ]);
 });
 
 // halamn single post
 Route::get('post/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Diki Rahman",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magni dolorum neque ut, quaerat, hic esse itaque autem dicta illo corrupti sit molestias quibusdam reprehenderit tenetur porro beatae inventore tempore aliquid obcaecati. Accusantium quis tempora accusamus laudantium sed voluptatem debitis aliquam dolores. Assumenda adipisci molestiae error pariatur recusandae doloribus itaque ratione nemo placeat optio debitis, aliquid animi illo quisquam dicta quasi eos saepe, atque quo repudiandae, laboriosam cumque rerum et! Nulla quo quidem earum! Omnis similique accusamus dicta rerum laudantium?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Diki Rahman",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magni dolorum neque ut, quaerat, hic esse itaque autem dicta illo corrupti sit molestias quibusdam reprehenderit tenetur porro beatae inventore tempore aliquid obcaecati. Accusantium quis tempora accusamus laudantium sed voluptatem debitis aliquam dolores. Assumenda adipisci molestiae error pariatur recusandae doloribus itaque ratione nemo placeat optio debitis, aliquid animi illo quisquam dicta quasi eos saepe, atque quo repudiandae, laboriosam cumque rerum et! Nulla quo quidem earum! Omnis similique accusamus dicta rerum laudantium? laboriosam cumque rerum et! Nulla quo quidem earum! Omnis similique accusamus dicta rerum laudantium?"
-        ],
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }    
-
+     
     return view('post', [
         "title" => "Single Post",
-        "post" => $new_post
+        "post" => Post::find($slug)
     ]);
 });
